fddfeaf941fc0ea30edf57ef5b7c321997b6472d
Jason Force <jason@mojason.com>
2016-03-15 20:05:14 -0500

    WIP Map w/o iframe, add simple-sidebar asset

--- js/map.js
+++ js/map.js
@@ -5,7 +5,7 @@ function initGoogleMap() {
   var littleBlueLatLong = new google.maps.LatLng(39.034521,-94.351333);
   var costcoLatLong = new google.maps.LatLng(39.036698,-94.357593);
 
-  var googleMap = new google.maps.Map(document.getElementById("map-area"), {
+  var googleMap = new google.maps.Map(document.getElementById("map"), {
     center: {lat: 39.035, lng: -94.352},
     zoom: 15,
     fullscreenControl: true
@@ -15,31 +15,34 @@ function initGoogleMap() {
     position: stroudsLatLong,
     map: googleMap,
     title: "Stroud's"
+    // TODO use Yelp for review
   });
 
   var cornerCafeMarker = new google.maps.Marker({
     position: cornerCafeLatLong,
     map: googleMap,
     title: "Corner Cafe"
+    // TODO use Yelp for review
   });
 
   var naturalGrocersMarker = new google.maps.Marker({
     position: naturalGrocersLatLong,
     map: googleMap,
     title: "Natural Grocers"
+    // TODO find API for info source
   });
 
   var littleBlueMarker = new google.maps.Marker({
     position: littleBlueLatLong,
     map: googleMap,
     title: "Little Blue River"
-    // use wikipedia API for info on river
+    // TODO use wikipedia API for info on river
   });
 
   var costcoMarker = new google.maps.Marker({
     position: costcoLatLong,
     map: googleMap,
     title: "Costco"
-    // use wikipedia API for info on costco, maybe yelp for a review on the eatery
+    // TODO use wikipedia API for info on costco, maybe yelp for a review on the eatery
   });
 };
