07c0bdf710c862db84ef9eeae9d98a77220e0a75
Jason Force <jason@mojason.com>
2016-03-31 22:11:10 -0500

    Refactor AJAX to one code block.

--- js/app.js
+++ js/app.js
@@ -2,35 +2,35 @@
 // and list items / buttons.
 var places = ko.observableArray([
   {
-    lat: 39.039148,
-    lng: -94.348433,
-    title: "Stroud's",
-    id: 'strouds'
+    lat: 39.040008,
+    lng: -94.357552,
+    title: "Silverstein Eye Centers Arena",
+    id: 'silverstein-arena',
   },
   {
     lat: 39.035531,
     lng: -94.341660,
     title: 'Corner Cafe',
-    id: 'corner-cafe'
+    id: 'corner-cafe',
   },
   {
     lat: 39.030588,
     lng: -94.357518,
-    title: 'Natural Grocers',
+    title: 'Vitamin Cottage',
     infoWindow: '',
-    id: 'natural-grocers'
+    id: 'vitamin-cottage',
   },
   {
     lat: 39.034521,
     lng: -94.351333,
     title: 'Little Blue River',
-    id: 'little-blue-river'
+    id: 'little-blue-river',
   },
   {
     lat: 39.036698,
     lng: -94.357593,
     title: 'Costco',
-    id: 'costco'
+    id: 'costco',
   }
 ]);
 
@@ -49,6 +49,11 @@ function initGoogleMap() {
     streetViewControl: false
   });
 
+  var $getWikiInfo = function(data) {
+    console.log(data);
+
+}
+
   // Loop over markers array creating new map markers.
   places().forEach(function(data) {
     var mkPosition = new google.maps.LatLng(data.lat, data.lng);
@@ -60,107 +65,34 @@ function initGoogleMap() {
     });
 
     var listButton = $('#' + data.id);
-    var infoWindow = new google.maps.InfoWindow({
-      content: data.contentString
+
+    var infoWindow;
+
+    $.ajax({
+      url: 'https://en.wikipedia.org/w/api.php?format=json&action=opensearch&search=' + data.title + '&callback=wikiCallback',
+      dataType: 'jsonp',
+      jsonp: 'callback',
+      headers: {'Api-User-Agent': 'jforce/udacity-project/jason@mojason.com'},
+      success: function(response) {
+        var linkString = "<div class='map-info-window'><h5>" + data.title + "</h5>" +
+                         "<p>" + response[2][0] + "</p>";
+        response[3].forEach(function(entry) {
+          linkString += '<a href="' + entry + '" target=_blank>' + entry + '</a><br>';
+        });
+        linkString += "</div>"
+        console.log(response);
+        data.contentString = linkString;
+      }
+    }).fail(function() {
+      data.contentString = 'Oops!  Looks like there was an issue getting data from Wikipedia.  Try refreshing the page.';
+    }).done(function() {
+      infoWindow = new google.maps.InfoWindow({
+        content: data.contentString
+      });
     });
 
-    if (data.title === "Stroud's") {
-      var linkString = "<div class='map-info-window'><h5>Stroud's Restaurant</h5>" +
-      "<p>The year was 1933. Prohibition had just been repealed, " +
-      "we were between two World Wars and Stroud’s Restaurant was built.<br>" +
-      "--Quoted from Stroud's website.</p>" +
-      "<a href='http://independence.stroudsrestaurant.com/' target=_blank>Click here for stroudsrestaurant.com</a></div>";
-      infoWindow.setContent(linkString);
-    }
 
-    if (data.title === 'Corner Cafe') {
-      var linkString = "<div class='map-info-window'><h5>Corner Cafe</h5>" +
-      "<p>There had always been a small restaurant at the ‘Corner’ in Riverside Missouri, " +
-      "but before 1983 it had changed names and hands several times. Many of the locals " +
-      "wanted to see a return to the old style cafe that had existed there years before and " +
-      "was an icon of its time (Reese’s Cafe).<br>" +
-      "--Quoted from Corner Cafe's website.</p><br>" +
-      "<a href='http://www.thecornercafe.com/' target=_blank>Click here for thecornercafe.com</a></div>";
-      infoWindow.setContent(linkString);
-    }
 
-    if (data.title === 'Natural Grocers') {
-      $.ajax({
-        url: 'https://en.wikipedia.org/w/api.php?format=json&action=opensearch&search=vitamin%20cottage%20natural%20grocers&callback=wikiCallback',
-        dataType: 'jsonp',
-        jsonp: 'callback',
-        headers: {'Api-User-Agent': 'jforce/udacity-project/jason@mojason.com'},
-        success: function(response) {
-          var linkString = "<div class='map-info-window'><h5>Natural Grocers</h5>" +
-                           "<p>Our company has been in business since 1955. We are " +
-                           "a company that is built on great intent and integrity. " +
-                           "From the beginning we established a foundation we refer " +
-                           "to as our Five Founding Principles. <br>" +
-                           "--Quoted from Natural Grocers website</p>" +
-                           "<p>Want to learn a little about Natural Grocers?</p>" +
-                           "<a href='https://www.naturalgrocers.com/about/our-five-founding-principles/' target=_blank>https://www.naturalgrocers.com/about/our-five-founding-principles/</a><br>" +
-                           "<a href='https://www.naturalgrocers.com/about/the-natural-grocers-story/' target=_blank>https://www.naturalgrocers.com/about/the-natural-grocers-story/</a><br>";
-          response[3].forEach(function(entry) {
-            linkString += '<a href="' + response[3] + '" target=_blank>' + entry + '</a><br>';
-          });
-          linkString += "</div>"
-          infoWindow.setContent(linkString);
-        },
-        fail: function() {
-          infoWindow.setContent('Oops!  Looks like there was an issue getting data from Wikipedia.  Try refreshing the page.');
-        }
-      });
-    }
-
-    if (data.title === 'Little Blue River') {
-      $.ajax({
-        // The commented out url will return data that can be used to get the actual wiki article content.
-        // However, without using some fancy regex to parse only the bits you want from the 'extract', you
-        // end up creating a very large infowindow when you stick that content into it.
-        //url: 'https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions%7Cextracts%7Clinks&titles=Little_Blue_River_(Missouri)&redirects=1&callback=wikiCallback&utf8=1&rvprop=content&rvparse=1&exsectionformat=plain',
-        url: 'https://en.wikipedia.org/w/api.php?format=json&action=opensearch&search=little%20blue%20river%20MO&callback=wikiCallback',
-        dataType: 'jsonp',
-        jsonp: 'callback',
-        headers: {'Api-User-Agent': 'jforce/udacity-project/jason@mojason.com'},
-        success: function(response) {
-          var linkString = "<div class='map-info-window'><h5>Battle of Little Blue River, American Civil War</h5>" +
-                           "<p>The Battle of Little Blue River was a battle of the American Civil War," +
-                           "occurring on October 21, 1864 in Jackson County, Missouri during Price's Raid. " +
-                           "</p><p>Learn more by clicking on the links below.</p>" +
-                           "<a href='https://www.nps.gov/abpp/battles/mo024.htm' target=_blank>https://www.nps.gov/abpp/battles/mo024.htm</a><br>";
-          response[3].forEach(function(entry) {
-            linkString += '<a href="' + response[3] + '" target=_blank>' + entry + '</a>';
-          });
-          linkString += "</div>"
-          infoWindow.setContent(linkString);
-        },
-        fail: function() {
-          infoWindow.setContent('Oops!  Looks like there was an issue getting data from Wikipedia.  Try refreshing the page.');
-        }
-      });
-    }
-
-    if (data.title === 'Costco') {
-      $.ajax({
-        url: 'https://en.wikipedia.org/w/api.php?format=json&action=opensearch&search=costco&callback=wikiCallback',
-        dataType: 'jsonp',
-        jsonp: 'callback',
-        headers: {'Api-User-Agent': 'jforce/udacity-project/jason@mojason.com'},
-        success: function(response) {
-          var linkString = "<div class='map-info-window'><h5>Costco</h5>" +
-                           "<p>If you don't know what Costco is, click one of these links</p>" +
-                           "<a href='http://www.costco.com'>www.costco.com</a><br>";
-          response[3].forEach(function(entry) {
-            linkString += '<a href="' + response[3] + '" target=_blank>' + entry + '</a><br>';
-          });
-          linkString += "</div>"
-          infoWindow.setContent(linkString);
-        },
-        fail: function() {
-          infoWindow.setContent('Oops!  Looks like there was an issue getting data from Wikipedia.  Try refreshing the page.');
-        }
-      });
-    }
 
     // Set the marker we just created as a property on the list
     // we just looped over so we can have it available for later.
