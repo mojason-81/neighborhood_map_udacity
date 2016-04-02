1f4d07edcb6f9cfa9214738b9f3309e7f0f346b8
Jason Force <jason@mojason.com>
2016-04-01 21:35:33 -0500

    Add comments, wrap up filter functionality

--- js/app.js
+++ js/app.js
@@ -34,8 +34,6 @@ var places = ko.observableArray([
   }
 ]);
 
-var mapMarkersExist = false;
-
 // Initialize Google Map
 function initGoogleMap() {
 
@@ -45,7 +43,7 @@ function initGoogleMap() {
       lat: 39.036018,
       lng: -94.353780
     },
-    zoom: 16,
+    zoom: 15,
     fullscreenControl: true,
     zoomControl: false,
     streetViewControl: false
@@ -53,7 +51,15 @@ function initGoogleMap() {
 
   var ajaxFailureCount = 0;
 
-  // googleMap.addListener('click', function() {do stuff});
+  // TODO Future enhancment. Add only one click event
+  // to the map.  Clicking the markers should 'bubble up'
+  // to the map and they can be handled there dependent
+  // on event.eventTarget
+  // Example:
+  // googleMap.addListener('click', function(event) {
+  //   console.log(event.eventTarget)
+  //   do more stuff;
+  // });
 
   // Loop over markers array creating new map markers.
   places().forEach(function(data) {
@@ -65,10 +71,11 @@ function initGoogleMap() {
       animation: google.maps.Animation.DROP,
     });
 
-    var listButton = $('#' + data.id);
-
     var infoWindow;
 
+    // TODO Future enhancement. Assign this ajax call as a variable outside the
+    // forEach() loop. On marker clicks, pass in the marker and make the ajax
+    // call then.
     $.ajax({
       url: 'https://en.wikipedia.org/w/api.php?format=json&action=opensearch&search=' + data.title + '&callback=wikiCallback',
       dataType: 'jsonp',
@@ -84,6 +91,9 @@ function initGoogleMap() {
         data.contentString = linkString;
       }
     }).fail(function() {
+
+      // Don't want to alert the user 5 times if API calls to Wikipedia are failing.  Once is enough
+      // to indicate there's an issue.
       if (ajaxFailureCount === 0) {
         alert('Oops!  Looks like there was an issue getting data from Wikipedia.  Try refreshing the page.');
         ajaxFailureCount++;
@@ -94,9 +104,6 @@ function initGoogleMap() {
         content: data.contentString
       });
     });
-
-
-
 
     // Set the marker we just created as a property on the list
     // we just looped over so we can have it available for later.
@@ -190,7 +197,6 @@ function ViewModel() {
   this.placeList = ko.observableArray([]);
 
   places().forEach(function(place) {
-    // console.log(place);
     place.visible = ko.observable(true);
     self.placeList.push(place);
   });
@@ -212,24 +218,24 @@ function ViewModel() {
   // input into the filter text input.
   this.filterList = ko.computed(function() {
     places().forEach(function(place) {
+      var searchParam = self.filterValue().toLowerCase();
+      var toBeSearched = place.title.toLowerCase();
 
-      // Check for existence of mapMarker since on initial render, the marker
-      // doesn't yet exist by the time filterList() is called / built.
-      if (place.mapMarker) {
-        var searchParam = self.filterValue().toLowerCase();
-        var toBeSearched = place.title.toLowerCase();
+      // Set visible binding to the folowing boolean.  Filters on type.
+      place.visible(!toBeSearched.indexOf(searchParam) || !searchParam);
 
-        // Set visible binding to the folowing boolean.  Filters on type.
-        place.visible(!toBeSearched.indexOf(searchParam) || !searchParam);
+      // Have to add check for existence of mapMarker since on initial render,
+      // the marker doesn't yet exist by the time filterList() is called / built.
+      if (place.visible() && searchParam && place.mapMarker) {
+        place.triggerMarker(place.mapMarker)
+      }
 
-        if (place.visible() && searchParam) {
-          place.triggerMarker(place.mapMarker)
-        }
-
+      // Make sure to close all infowindows if the filter doesn't have a match.
+      else if (place.mapMarker) {
+        place.closeInfoWindow();
       }
     })
   }, this);
-
 }
 
 ko.applyBindings(new ViewModel());
